66ed46f9cc2dc27ae3f8a1a699870974f125b492
Biboys999 <mohamedrayane.aboussaid@gmail.com>
2020-02-15 01:01:16 +0100

    jeu de devinette opérationel !

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-
-
 var aleatoire = Math.floor((Math.random() * 10) + 1);
 console.log(aleatoire)
 
 let home = document.querySelector("#home");
 let win = document.querySelector("#win");
 let loose = document.querySelector("#loose");
-let chance2 = document.querySelector("#chance2");
-let chance1 = document.querySelector("#chance1");
+let plus = document.querySelector("#plus");
+let moins = document.querySelector("#moins");
+
+let hpm = home + plus + moins
 
 var btn = document.querySelector('#btn');
 
 let counter = 0
-
 
 btn.onclick = function() {
     var num1 = document.querySelector('#text-input').value;
@@ -20,22 +19,35 @@ btn.onclick = function() {
 
     if (num1 == aleatoire) {
         home.style.display = "none";
+        moins.style.display = "none";
+        plus.style.display = "none";
+
         win.style.display = "block";
     }
-    else{
-        home.style.display = "none";
-        chance2.style.display = "block";
-        counter++;
-        if (counter == 3) {
-        home.style.display = "none";
-        loose.style.display = "block";
+    else if(counter == 2) {
+        moins.style.display = "none";
+        plus.style.display = "none";
 
-        }
+        loose.style.display = "block";
+    }
+    else if(num1 > aleatoire){
+        home.style.display = "none";
+        plus.style.display = "none";
+
+        moins.style.display = "block";
+        counter++
+    }
+    else if(num1 < aleatoire){
+        home.style.display = "none";
+        moins.style.display = "none";
+
+        plus.style.display = "block";
+        counter++
     }
 }
 
-/*
-btn3.onclick = function(){
+let restart = document.querySelector("#restart");
+
+restart.onclick = function(){
     location.reload();
 }
-*/
